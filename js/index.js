2670482298c82658dd97dbc359cfb66b2a4f6547
Deep_learning <128953886+Hyperthymesia-X@users.noreply.github.com>
2025-03-11 00:30:08 +0800

    Birthday

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="hxy" &&  pwd=="0318"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("姓名简写，密码猜错了哦，想想今天什么日子o.O");
+	}
+});
